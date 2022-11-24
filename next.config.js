3abf7767eb165803bffea2c4d32a5e1a91c682ff
Media3rt <A.95mb@yahoo.com>
2022-11-24 10:47:58 +0400

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images:{
+    loader: "akamai",
+    path: "",
+  }
 }
 
+
 module.exports = nextConfig
+
