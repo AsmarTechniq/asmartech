e5b1227477af0571c67c42a12e7f3d402697cf7a
Media3rt <A.95mb@yahoo.com>
2024-02-06 13:58:50 +0400

    update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,9 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import propertyImg from '../public/assets/projects/property.jpg';
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import cryptoImg from '../public/assets/projects/crypto.jpg';
+import netflixImg from '../public/assets/projects/netflix.jpg';
+import twitchImg from '../public/assets/projects/twitch.jpg';
+import aqmishaImg from '../public/assets/projects/aqmisha.jpg';
 import ProjectItem from './ProjectItem';
 import ProjectTag from './ProjectTag';
 
@@ -91,6 +92,13 @@ const Projects = () => {
             backgroundImg={twitchImg}
             projectUrl='/Novemberfurniture'
             tech='Wordpress'
+
+          />
+          <ProjectItem
+            title='Alaqmisha'
+            backgroundImg={aqmishaImg}
+            projectUrl='/Alaqmisha'
+            tech='Ui/Ux'
 
           />
         </div>
